23ee07e35150e6a2ffe3b89d77db8dfaa6f13fb2
Cosmodocus <149424191+Cosmodocus@users.noreply.github.com>
2024-02-01 21:31:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import envCompatible from 'vite-plugin-env-compatible';
 
 
-dotenv.config({ path: `.env.${process.env.TMDB_API_KEY}` });
+// dotenv.config({ path: `.env.${process.env.TMDB_API_KEY}` });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
     envCompatible(),
   ],
   // to test if file is development or production
-  build: {
-    minify: mode === 'production',
-  }
+  // build: {
+  //   minify: mode === 'production',
+  // }
 });
